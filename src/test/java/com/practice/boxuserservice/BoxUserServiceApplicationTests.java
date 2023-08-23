395467153cf679d2bf73d4eb21422bbcabfc39ec
middlefitting <wjdtmdcjf2@gmail.com>
2023-08-23 16:29:45 +0900

    feat(root): 기본 환경설정

---
+++ src/test/java/com/practice/boxuserservice/BoxUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.boxuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxUserServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
